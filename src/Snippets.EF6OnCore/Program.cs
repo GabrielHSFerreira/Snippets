fa6d6ad79d3cb70060a2160376695841eb0bd330
GabrielHSFerreira <ghenrique_10@hotmail.com>
2020-03-06 10:22:16 -0300

    Entity Framework 6 working on .NET Core 3.1 project snippet.

---
+++ src/Snippets.EF6OnCore/Program.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Data.Entity;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Snippets.EF6OnCore
+{
+    public static class Program
+    {
+        public async static Task Main()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            using var context = new DataContext(configuration.GetConnectionString("DefaultConnection"));
+
+            context.Entities.Add(new Entity { Value = "Some value" });
+            context.Entities.Add(new Entity { Value = "Another value" });
+            await context.SaveChangesAsync();
+
+            var entities = await context.Entities.ToListAsync();
+            foreach (var entity in entities)
+                Console.WriteLine(entity);
+        }
+    }
+
+    public class DataContext : DbContext
+    {
+        public DbSet<Entity> Entities { get; set; }
+
+        public DataContext(string connectionString) : base(connectionString) { }
+    }
+
+    public class Entity
+    {
+        public int Id { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Id} - {Value}";
+        }
+    }
+}
